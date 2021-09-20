cdd68b287849046bff5f9584e2a91afcec56487c
arland <arland.torres@outlook.com>
2021-09-20 22:49:50 +0100

    chore(setup): setup project & deploy to gh-pages
    dev dependencies: sveltekit, tailwind-css, commitlint, gh-pages

---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/portfolio' : ''
+		}
+	}
+};
+
+export default config;
